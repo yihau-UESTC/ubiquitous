42e1ec797563046c5a4b56ef9ef31042ed5f866f
yihau <yihau1020@163.com>
2018-02-01 21:45:47 +0800

    NettyTimeServer

---
+++ src/main/java/NettyGuideDemo/chapter2/AIOTimeClientDemo.java
@@ -0,0 +1,8 @@
+package NettyGuideDemo.chapter2.chapter2;
+
+public class AIOTimeClientDemo {
+    public static void main(String[] args) {
+        int port = 8888;
+        new Thread(new AsyncTimeClientHandler("127.0.0.1", port)).start();
+    }
+}
---
+++ src/main/java/NettyGuideDemo/chapter2/AsyncTimeClientHandler.java
@@ -0,0 +1,109 @@
+package NettyGuideDemo.chapter2.chapter2;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.AsynchronousSocketChannel;
+import java.nio.channels.CompletionHandler;
+import java.util.concurrent.CountDownLatch;
+
+public class AsyncTimeClientHandler implements CompletionHandler<Void, AsyncTimeClientHandler>, Runnable {
+    private AsynchronousSocketChannel asynchronousSocketChannel;
+    private String host;
+    private int port;
+    private CountDownLatch latch;
+
+    public AsyncTimeClientHandler(String s, int port) {
+        this.host = s;
+        this.port = port;
+        try {
+            asynchronousSocketChannel = AsynchronousSocketChannel.open();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void run() {
+        latch = new CountDownLatch(1);
+        asynchronousSocketChannel.connect(new InetSocketAddress(host, port), this, this);
+        try {
+            latch.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        try {
+            asynchronousSocketChannel.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void completed(Void result, AsyncTimeClientHandler attachment) {
+        byte[] req = "QUERY TIME ORDER".getBytes();
+        ByteBuffer writeBuffer = ByteBuffer.allocate(req.length);
+        writeBuffer.put(req);
+        writeBuffer.flip();
+        asynchronousSocketChannel.write(writeBuffer, writeBuffer, new CompletionHandler<Integer, ByteBuffer>() {
+            @Override
+            public void completed(Integer result, ByteBuffer attachment) {
+                if (writeBuffer.hasRemaining()) {
+                    asynchronousSocketChannel.write(attachment, attachment, this);
+                } else {
+                    ByteBuffer readBuffer = ByteBuffer.allocate(1024);
+                    asynchronousSocketChannel.read(readBuffer, readBuffer, new CompletionHandler<Integer, ByteBuffer>() {
+                        @Override
+                        public void completed(Integer result, ByteBuffer attachment) {
+                            readBuffer.flip();
+                            byte[] bytes = new byte[attachment.remaining()];
+                            attachment.get(bytes);
+                            String body = "";
+                            try {
+                                body = new String(bytes, "UTF-8");
+                                System.out.println("Now is : " + body);
+                                latch.countDown();
+                            } catch (UnsupportedEncodingException e) {
+                                e.printStackTrace();
+                            }
+                        }
+
+                        @Override
+                        public void failed(Throwable exc, ByteBuffer attachment) {
+                            try {
+                                asynchronousSocketChannel.close();
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            } finally {
+                                latch.countDown();
+                            }
+                        }
+                    });
+                }
+            }
+
+            @Override
+            public void failed(Throwable exc, ByteBuffer attachment) {
+                try {
+                    asynchronousSocketChannel.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } finally {
+                    latch.countDown();
+                }
+            }
+        });
+    }
+
+    @Override
+    public void failed(Throwable exc, AsyncTimeClientHandler attachment) {
+        try {
+            asynchronousSocketChannel.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            latch.countDown();
+        }
+    }
+}
